8084189600d644fe31e4b66e8ea13998ae4ed7c1
Jorge Epuñan <juanbrujo@gmail.com>
2024-06-21 14:19:10 -0300

    chore: new structure and bash script

--- script.sh
+++ script.sh
@@ -22,46 +22,34 @@ while getopts "p:f" opt; do
     esac
 done
 
-dir=$(dirname "$file")
+# dir=$(dirname "$file")
+dir="./public"
+assets="./assets"
 
 # Check if $dir/index.html exists, if yes, skip this file
-if [[ -f "$dir/index.html" ]]; then
-    if [[ -n $FORCE_PROCESSING ]]; then
-        echo "Forcing re-processing of HTML version"
-    else
-        echo "HTML version exists. Skipping processing..."
-        exit
-    fi
+if [[ "$dir" ]]; then
+    echo "Directory exists. Deleting all..."
+    rm -rf "$dir"
+    mkdir "$dir"
 fi
 
-# Step1: convert PDF to images
+# Convert PDF to images
 # pdftoppm will extract each page as a png
-echo -n "#1: Exporting $file pages as PNG to $dir ... "
-pdftoppm "$file" "${dir}"/slide -png
+echo -n "Exporting PDF pages as PNG to $dir ... "
+pdftoppm "${assets}"/pdf/*.pdf "${dir}"/slide -png
 echo "DONE"
 
-# Step2: compress PNG images for the web
-# pngquant will compress images
-# -s 1 uses aggressive compression
-# -f forces overwriting output files if they exist (for us its same as input)
-###echo -n "#2: Compressing PNG files ... "
-###for f in "${dir}"/*.png; do
-###    pngquant "$f" -s 1 -f -o "$f"
-###done
-###echo "DONE"
+# Copy assets to directory
+# mkdir "${dir}/revealjs"
+cp -r "${assets}"/revealjs "${dir}"
 
-# # Step3: insert link to images in HTML template
-# # this link is hardcoded for convenience
-cp ./template.html "${dir}"/index.html
-echo -n "#3: inserting links to PNG in reveal.js HTML ... "
+# Insert link to images in HTML template
+cp "${assets}"/template.html "${dir}"/index.html
+echo -n "Creating HTML ... "
 mapfile -t slides < <(find "${dir}" -iname '*.png' | sort -r)
 for f in "${slides[@]}"; do
     f=$(basename "$f")
     img="<section><img src='$f'></section>"
-    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        sed -i "16i $img" "${dir}"/index.html 
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-        gsed -i "16i $img" "${dir}"/index.html
-    fi
+    gsed -i "16i $img" "${dir}"/index.html
 done;
 echo "DONE"
